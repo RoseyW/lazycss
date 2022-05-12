2760474646e8778282e65e4339c822ef0a116f27
spaler@163.com <roseywrong@163.com>
2022-05-12 17:36:28 +0800

    first commit 0.1.1

---
+++ rollup.config.ts
@@ -0,0 +1,32 @@
+import typescript from 'rollup-plugin-typescript2';
+
+const mode = process.env.MODE;
+const isProd = mode === 'prod';
+const pkg = require('./package.json');
+
+export default {
+    input: `src/index.ts`,
+    output: [
+        {
+            file: pkg.main,
+            exports: 'named',
+            format: 'cjs',
+            sourcemap: !isProd
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: !isProd
+        },
+        {
+            file: 'build/lazyCss.global.js',
+            name: 'lazyCss',
+            format: 'iife',
+            sourcemap: !isProd
+        },
+    ],
+    plugins: [typescript({
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: { compilerOptions: { sourceMap: !isProd } }
+    })],
+};
